3557696f48c96aa01ea8c68f830e953d955db3e3
Christian Tukunda <christiantukundastocklin@gmail.com>
2024-08-11 21:23:51 +0100

    first commit

--- server.js
+++ server.js
@@ -197,12 +197,9 @@ const seedData = async () => {
 // seedData();
 
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5500;
 
-// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 
 // const express = require('express');
