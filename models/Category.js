46086d88d3108573650d9dab8d315a5a4c56f0e9
Christian Tukunda <christiantukundastocklin@gmail.com>
2024-08-15 16:01:25 +0100

    first commit'

--- models/Category.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const categorySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }]
-});
-
-module.exports = mongoose.model('Category', categorySchema);
--- routes/categoryRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const categoryController = require('../controllers/categoryController');
-const { authMiddleware, adminMiddleware } = require('../middleware/authMiddleware');
-
-router.post('/', authMiddleware, adminMiddleware, categoryController.createCategory);
-router.get('/', authMiddleware, categoryController.getCategories);
-
-module.exports = router;
